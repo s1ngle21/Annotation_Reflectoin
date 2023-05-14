99b2c1798d00b7498e531777d563ef874ef9cb60
s1nglev21 <st1mxxd@gmail.com>
2023-05-14 12:14:12 +0300

    Add task

---
+++ src/homework_task/MethodTests.java
@@ -0,0 +1,33 @@
+package homework_task;
+
+import homework_task.annotations.AfterSuite;
+import homework_task.annotations.BeforeSuite;
+import homework_task.annotations.Test;
+
+public class MethodTests {
+
+    @BeforeSuite
+    public static void beforeMethod() {
+        System.out.println("Running before suite");
+    }
+
+    @Test(priority = 1)
+    public static void test1() {
+        System.out.println("Running test1");
+    }
+
+    @Test(priority = 2)
+    public static void test2() {
+        System.out.println("Running test3");
+    }
+
+    @Test(priority = 3)
+    public static void test3() {
+        System.out.println("Running test3");
+    }
+
+    @AfterSuite
+    public static void afterMethod() {
+        System.out.println("Running after suite");
+    }
+}
---
+++ src/homework_task/TestRunner.java
@@ -0,0 +1,63 @@
+package homework_task;
+
+import homework_task.annotations.AfterSuite;
+import homework_task.annotations.BeforeSuite;
+import homework_task.annotations.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class TestRunner {
+
+    public static void start(Class<?> clazz) {
+        List<Method> beforeSuiteMethods = new ArrayList<>();
+        List<Method> testMethods = new ArrayList<>();
+        List<Method> afterSuiteMethod = new ArrayList<>();
+
+        Method[] methods = clazz.getDeclaredMethods();
+
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(BeforeSuite.class)) {
+                beforeSuiteMethods.add(method);
+            } else if (method.isAnnotationPresent(Test.class)) {
+                testMethods.add(method);
+            } else if (method.isAnnotationPresent(AfterSuite.class)) {
+                afterSuiteMethod.add(method);
+            }
+        }
+
+        if (beforeSuiteMethods.size() > 1 && afterSuiteMethod.size() > 1) {
+            throw new RuntimeException("Methods with @homework_task.annotations.BeforeSuite and @homework_task.annotations.AfterSuite annotations must be in a single instance!");
+        }
+
+        if (!beforeSuiteMethods.isEmpty()) {
+            try {
+                beforeSuiteMethods.get(0).invoke(clazz.newInstance());
+            } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        testMethods.stream()
+                .sorted(Comparator.comparing(method1 -> method1.getAnnotation(Test.class).priority()));
+        for (Method method : testMethods) {
+            try {
+                method.invoke(clazz.newInstance());
+            } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        if (!afterSuiteMethod.isEmpty()) {
+            try {
+                afterSuiteMethod.get(0).invoke(clazz.newInstance());
+            } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
